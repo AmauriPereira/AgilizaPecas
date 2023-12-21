fc2dab08b83cd6f2ba51270c3c6c0c1030930a5e
AmauriPereira <amauri.pj@hotmail.com>
2023-12-21 08:12:42 -0300

    Criação de pacotes
    Foi criaado o pacote dados, utilitarios, excessooes, negocio, icones.

---
+++ AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/negocios/File.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.minaspecas.irmaospereira.agilizapecas.negocios;
+
+
+/**
+ *
+ * @author Dinopc
+ */
+public class File {
+    
+}
