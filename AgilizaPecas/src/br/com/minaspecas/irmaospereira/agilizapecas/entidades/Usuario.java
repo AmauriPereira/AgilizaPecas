3f4b003409a3c609dc40c0a01725e3cd3d32836f
AmauriPereira <amauri.pj@hotmail.com>
2023-12-24 13:28:00 -0300

    Update Usuario.java
    Atualização na clalsse usuario

--- AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/entidades/Usuario.java
+++ AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/entidades/Usuario.java
@@ -7,6 +7,40 @@ package br.com.minaspecas.irmaospereira.agilizapecas.entidades;
  */
 public class Usuario {
     private int idUsuario;
-    private int usuario;
-    
+    private String usuario;
+    private String senha;
+    private String ativo;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(String ativo) {
+        this.ativo = ativo;
+    }
+      
 }
