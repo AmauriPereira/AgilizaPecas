5f44b353d505c1433c503a0b511eec6aeea4604f
AmauriPereira <amauri.pj@hotmail.com>
2023-12-24 13:26:33 -0300

    Update LoginDAO.java
    Atualizacao no LoginDAO - Simplificado

--- AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/dados/LoginDAO.java
+++ AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/dados/LoginDAO.java
@@ -1,10 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package br.com.minaspecas.irmaospereira.agilizapecas.dados;
 
 import br.com.minaspecas.irmaospereira.agilizapecas.entidades.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -12,12 +12,54 @@ import java.sql.SQLException;
  * @author Amauri Pereira
  */
 public class LoginDAO {
-    private static final String SQL_SELECT_ACESSO = "SELECT * FROM ACESSO";
     
-    public Usuario selectUsuario () throws SQLException{
+    private static final String SQL_SELECT_ACESSO = "SELECT ID_USUARIO, USUARIO, SENHA, ATIVO FROM ACESSO WHERE USUARIO LIKE ? AND SENHA LIKE ? AND ATIVO = 'S'";
+    
+    public Usuario selectUsuario (String usuario, String senha) throws SQLException{
         
-        return null;
+        Connection conexao = null;
+        PreparedStatement comando = null;
+        ResultSet resultado = null;
+        Usuario user = null;
         
+        try {
+            
+            conexao = BancoDadosUtil.getConnection();
+            
+            conexao.setAutoCommit(false);
+
+            comando = conexao.prepareStatement(SQL_SELECT_ACESSO);
+            comando.setString(1, usuario);
+            comando.setString(2, senha);
+
+            resultado = comando.executeQuery();
+
+            if (resultado.next()) {
+                user = new Usuario();
+
+                user.setIdUsuario(resultado.getInt("ID_USUARIO"));
+                user.setUsuario(resultado.getString("USUARIO"));
+                user.setSenha(resultado.getString("SENHA"));
+                user.setAtivo(resultado.getString("ATIVO"));
+
+            }
+            conexao.commit();
+            conexao.setAutoCommit(true);
+
+        } catch (Exception e) {
+            if (conexao != null) {
+                conexao.rollback();
+            }
+        } finally {
+            if (comando != null && !comando.isClosed()) {
+                comando.close();
+            }
+            if (conexao != null && !conexao.isClosed()) {
+                conexao.close();
+            }
+        }
+        return user;
     }
     
+   
 }
