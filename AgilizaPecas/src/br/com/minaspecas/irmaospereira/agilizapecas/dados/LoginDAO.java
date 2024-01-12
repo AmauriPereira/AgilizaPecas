952986fba2b5d263a9c8e0e908ba5d33cf5a4b29
AmauriPereira <amauri.pj@hotmail.com>
2024-01-11 23:50:00 -0300

    Update LoginDAO.java
    foi exluido os metodos insert_novo_usuario  e selectBuscaUsuario .
    E excluido tambem as consultas: SQL_SELECT_USUARIO , SQL_INSERT_NOVO_USUARIO  e SQL_ULTIMO_ID_USUARIO .

--- AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/dados/LoginDAO.java
+++ AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/dados/LoginDAO.java
@@ -14,10 +14,6 @@ import java.sql.SQLException;
 public class LoginDAO {
     
     private static final String SQL_SELECT_ACESSO = "SELECT ID_USUARIO, USUARIO, SENHA, ATIVO FROM ACESSO WHERE USUARIO LIKE ? AND SENHA LIKE ? AND ATIVO = 'S'";
-    private static final String SQL_SELECT_USUARIO = "SELECT * FROM ACESSO WHERE USUARIO LIKE ?";
-    private static final String SQL_INSERT_NOVO_USUARIO = "INSERT INTO USUARIO( ID_USUARIO,USUARIO, SENHA, ATIVO)VALUES (?,?,?,?)";
-    private static  final String SQL_ULTIMO_ID_USUARIO = "SELECT MAX(ID_USUARIO) FROM USUARIO";
-    
     
     public Usuario selectUsuario (String usuario, String senha) throws SQLException{
         
@@ -65,87 +61,4 @@ public class LoginDAO {
         return user;
     }
     
-    public void insert_novo_usuario (Usuario user) throws SQLException{
-        
-        Connection conexao = null;
-        PreparedStatement comando = null;
-        Integer ultimoId = null;
-        ResultSet resultado = null;
-        
-        try {
-            conexao = BancoDadosUtil.getConnection();
-            comando = conexao.prepareStatement(SQL_ULTIMO_ID_USUARIO);
-            resultado = comando.executeQuery();
-            
-            comando.setString(1, resultado.getString("ID_USUARIO"));
-            comando.setString(2, user.getUsuario());
-            comando.setString(3, user.getSenha());
-            comando.setString(4, user.getAtivo());
-            
-            comando = conexao.prepareStatement(SQL_INSERT_NOVO_USUARIO);
-
-            comando.execute();
-            conexao.commit();
-            
-        } catch (Exception e) {
-            if (conexao != null) {
-                conexao.rollback();
-            }
-        } finally {
-            if (comando != null && !comando.isClosed()) {
-                comando.close();
-            }
-            if (conexao != null && !conexao.isClosed()) {
-                conexao.close();
-            }
-        }
-        
-    } 
-    
-     public Usuario selectBuscaUsuario (String usuario) throws SQLException{
-        
-        Connection conexao = null;
-        PreparedStatement comando = null;
-        ResultSet resultado = null;
-        Usuario user = null;
-        
-        try {
-            
-            conexao = BancoDadosUtil.getConnection();
-            
-            conexao.setAutoCommit(false);
-
-            comando = conexao.prepareStatement(SQL_SELECT_USUARIO);
-            comando.setString(1, usuario);
-
-            resultado = comando.executeQuery();
-
-            if (resultado.next()) {
-                user = new Usuario();
-
-                user.setIdUsuario(resultado.getInt("ID_USUARIO"));
-                user.setUsuario(resultado.getString("USUARIO"));
-                user.setSenha(resultado.getString("SENHA"));
-                user.setAtivo(resultado.getString("ATIVO"));
-
-            }
-            conexao.commit();
-            conexao.setAutoCommit(true);
-
-        } catch (Exception e) {
-            if (conexao != null) {
-                conexao.rollback();
-            }
-        } finally {
-            if (comando != null && !comando.isClosed()) {
-                comando.close();
-            }
-            if (conexao != null && !conexao.isClosed()) {
-                conexao.close();
-            }
-        }
-        return user;
-    }
-    
-   
 }
