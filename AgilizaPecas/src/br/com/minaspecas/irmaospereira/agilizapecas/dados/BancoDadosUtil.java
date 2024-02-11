4673ce9f155e8cb02c799ed186d0a85ce9878e06
AmauriPereira <amauri.pj@hotmail.com>
2024-02-10 22:40:38 -0300

    Update BancoDadosUtil.java
    Atualização no caminho do banco.
    Agora esta pegando dinamicamente o caminho.

--- AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/dados/BancoDadosUtil.java
+++ AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/dados/BancoDadosUtil.java
@@ -4,20 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class BancoDadosUtil {
-    
-    //private static final String CAMINHO = System.getProperty("user.dir");    
-   //private static final String BANCO = "database/AGILIZA.FBD";    
+
     private static final String DRIVER = "org.firebirdsql.jdbc.FBDriver";
-    private static final String URL = "jdbc:firebirdsql://localhost/C:\\Users\\AMAURI PEREIRA\\Documents\\NetBeansProjects\\AgilizaPecas\\AgilizaPecas\\database\\AGILIZA.FDB";
+    private static final String URL = "jdbc:firebirdsql://localhost/" + System.getProperty("user.dir") + "\\database\\AGILIZA.FDB";
     private static final String USUARIO = "SYSDBA";
     private static final String SENHA = "masterkey";
+    
+     
+    
+    
 
 
     public static Connection getConnection() {
         // Configurações para a conexão com o banco de dados Firebird        
         Connection connection = null;
+        
+        
 
         try {
+
             Class.forName(DRIVER);
             connection = DriverManager.getConnection(URL,USUARIO,SENHA);
         } catch (Exception e) {
