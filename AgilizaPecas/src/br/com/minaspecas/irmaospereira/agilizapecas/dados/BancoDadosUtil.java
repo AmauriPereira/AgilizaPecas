f9322db5f9c161c51a534623ca77d8e36f2588d6
AmauriPereira <amauri.pj@hotmail.com>
2024-02-12 07:40:07 -0300

    Update BancoDadosUtil.java
    Exlcuido linhas em branco.

--- AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/dados/BancoDadosUtil.java
+++ AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/dados/BancoDadosUtil.java
@@ -6,7 +6,11 @@ import java.sql.DriverManager;
 public class BancoDadosUtil {
 
     private static final String DRIVER = "org.firebirdsql.jdbc.FBDriver";
+<<<<<<< Updated upstream
     private static final String URL = "jdbc:firebirdsql://localhost/" + System.getProperty("user.dir") + "\\database\\AGILIZA.FDB";
+=======
+    private static final String URL = "jdbc:firebirdsql://localhost/C:\\Users\\Dinopc\\Documents\\NetBeansProjects\\AgilizaPecas\\AgilizaPecas\\database\\AGILIZA.FDB";
+>>>>>>> Stashed changes
     private static final String USUARIO = "SYSDBA";
     private static final String SENHA = "masterkey";
 
