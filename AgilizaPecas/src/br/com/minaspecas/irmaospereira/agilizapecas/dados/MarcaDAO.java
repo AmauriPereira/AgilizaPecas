003a0617f308d74e0a46b55dfc7bba681bde9384
AmauriPereira <amauri.pj@hotmail.com>
2024-02-25 16:25:47 -0300

    Update MarcaDAO.java
    Inclusão do metodo IncluirMarca

--- AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/dados/MarcaDAO.java
+++ AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/dados/MarcaDAO.java
@@ -5,12 +5,12 @@
 package br.com.minaspecas.irmaospereira.agilizapecas.dados;
 
 import br.com.minaspecas.irmaospereira.agilizapecas.entidades.Marca;
-import br.com.minaspecas.irmaospereira.agilizapecas.entidades.Usuario;
-import br.com.minaspecas.irmaospereira.agilizapecas.excessoes.excessaoMarcaNaoEncontrada;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -18,7 +18,10 @@ import java.sql.SQLException;
  */
 public class MarcaDAO {
     
-    private static final String SQL_SELECT_MARCA = "SELECT * FROM MARCA WHERE DESCRICAO LIKE ?";
+    private static final String SQL_SELECT_MARCA = "SELECT * FROM MARCA WHERE NOME LIKE ?";
+    private static final String SQL_INSERT_NOVA_MARCA = "INSERT INTO MARCA ( ID_MARCA, NOME) VALUES (?,?)";
+    private static final String SQL_UPDATE_MARCA = "UPDATE MARCA SET NOME = ? WHERE ID_MARCA = ?";
+    private static final String SQL_GET_GENERATOR_ID_MARCA = "SELECT GEN_ID(GEN_MARCA_ID, 1) FROM RDB$DATABASE";
     
     public Marca pesquisaMarca (String marca) throws SQLException{
         
@@ -42,7 +45,7 @@ public class MarcaDAO {
                 marcaAux = new Marca();
 
                 marcaAux.setIdMarca(resultado.getInt("ID_MARCA"));
-                marcaAux.setDescricao(resultado.getString("DESCRICAO"));
+                marcaAux.setDescricao(resultado.getString("NOME"));
 
             }
             conexao.commit();
@@ -62,6 +65,76 @@ public class MarcaDAO {
             }
         }
         return marcaAux;
+    }
+    
+    public void incluiMarca (Marca marcaAux) throws SQLException {
+        
+        Connection conexao = null;
+        PreparedStatement comando = null;
+        
+        try {
+            
+            conexao = BancoDadosUtil.getConnection();            
+            conexao.setAutoCommit(false);
+            comando = conexao.prepareStatement(SQL_INSERT_NOVA_MARCA);
+            
+            comando.setInt(1,getGeneratorIdMarca());
+            comando.setString(2, marcaAux.getDescricao());
+
+            comando.executeUpdate();
+            conexao.setAutoCommit(true);
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao tentar atualizar marca: "+e, "Erro ao Atualizar", JOptionPane.ERROR_MESSAGE);
+            if (conexao != null) {
+                conexao.rollback();
+            }
+            throw new RuntimeException(e);
+
+        } finally {
+            if (comando != null && !comando.isClosed()) {
+                comando.close();
+            }
+            if (conexao != null && !conexao.isClosed()) {
+                conexao.close();
+            }
+        }
+    }
+    
+    private int getGeneratorIdMarca () throws SQLException{
+        
+        Connection conexao = null;
+        PreparedStatement comando = null;
+        ResultSet resultado = null;
+        Integer ultimoId = null;
+                
+        try {
+            
+            conexao = BancoDadosUtil.getConnection();            
+            conexao.setAutoCommit(false);
+            comando = conexao.prepareStatement(SQL_GET_GENERATOR_ID_MARCA);
+
+            resultado = comando.executeQuery();
+            
+            resultado.next();
+            ultimoId = resultado.getInt(1);
+           
+            conexao.commit();
+            conexao.setAutoCommit(true);
+
+        } catch (Exception e) {
+            if (conexao != null) {
+                conexao.rollback();
+            }
+        } finally {
+            if (comando != null && !comando.isClosed()) {
+                comando.close();
+            }
+            if (conexao != null && !conexao.isClosed()) {
+                conexao.close();
+            }
+        }
+        return ultimoId;
     }
     
 }
