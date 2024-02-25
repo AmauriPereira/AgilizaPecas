990003f46834d27fa6a963bf3f6b694eefe05bd7
AmauriPereira <amauri.pj@hotmail.com>
2024-02-25 16:23:17 -0300

    Update BancoDadosUtil.java
    Otimização da URL de conexão do banco.

--- AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/dados/BancoDadosUtil.java
+++ AgilizaPecas/src/br/com/minaspecas/irmaospereira/agilizapecas/dados/BancoDadosUtil.java
@@ -4,23 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class BancoDadosUtil {
-
+    
+    private static final String ARQUIVO = "\\database\\AGILIZA.FDB";
     private static final String DRIVER = "org.firebirdsql.jdbc.FBDriver";
-    private static final String URL = "jdbc:firebirdsql://localhost/" + System.getProperty("user.dir") + "\\database\\AGILIZA.FDB";
+    private static final String URL = "jdbc:firebirdsql://localhost/" + System.getProperty("user.dir") + ARQUIVO;
     private static final String USUARIO = "SYSDBA";
     private static final String SENHA = "masterkey";
     
-     
-    
-    
-
-
     public static Connection getConnection() {
         // Configurações para a conexão com o banco de dados Firebird        
         Connection connection = null;
         
-        
-
         try {
 
             Class.forName(DRIVER);
@@ -28,6 +22,7 @@ public class BancoDadosUtil {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+        
         return connection;
 
     }
